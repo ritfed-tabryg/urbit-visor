617f95bb6b596eef427a8dc54b972956213e54b2
mirtyl-wacdec <ycl2@protonmail.com>
2021-08-26 19:38:41 +0200

    Implemented simple modal window to replace i-frame

--- src/injected.ts
+++ src/injected.ts
@@ -15,11 +15,11 @@ function requestData(request: LWURequest, data: any = null) {
         rej(event.data.error);
       // reject promise if no ship is connected, i.e. extension is "locked"
       } else if(event.data == "locked"){
-        // unlock();
+        promptUnlock();
         rej("locked")
       // reject promise if there are no valid permissions
       } else if(event.data == "noperms"){
-        // promptPassword();
+        promptPerms();
         rej("noperms");
       }
       // resolve promise if permissions exist
@@ -32,36 +32,27 @@ function requestData(request: LWURequest, data: any = null) {
   });
 }
 
-// function unlock() {
-//   const background = document.getElementById("lwu-modal-bg")
-//   background.style.display = "block";
-//   const modal = document.getElementById("lwu-modal-fg");
-//   modal.innerText = "Please select a ship first."
-//   setTimeout(()=> background.style.display = "none", 1000);
-// }
-
-// function promptPassword(){
-//   const background = document.getElementById("lwu-modal-bg")
-//   background.style.display = "block";
-//   const modal = document.getElementById("lwu-modal-fg");
-//   modal.innerText= "You do not have permissions. Input your password to unlock Login With Urbit"
-//   const input = document.createElement('input');
-//   input.type = "password";
-//   input.id = "lwu-pw-input";
-//   const button = document.createElement("button");
-//   button.id = "submit-urbit-login";
-//   button.innerText = "Submit";
-//   modal.appendChild(input);
-//   modal.appendChild(button);
-//   button.onclick = () => {
-//     (window as any).urbit.test(input.value)
-//   }
-//   setTimeout(()=> background.style.display = "none", 1000);
-// }
+function promptUnlock(){
+  console.log('unlocking')
+  const background = document.getElementById("lwu-modal-bg")
+  background.style.display = "block";
+  background.style.opacity = "0.8";
+  const modalText = document.getElementById("urbit-visor-modal-text");
+  modalText.innerText = "Please connect to a ship first.";
+  setTimeout(()=> background.style.display = "none", 2000);
+}
+function promptPerms(){
+  console.log('unlocking')
+  const background = document.getElementById("lwu-modal-bg")
+  background.style.display = "block";
+  background.style.opacity = "0.8";
+  const modalText = document.getElementById("urbit-visor-modal-text");
+  modalText.innerText = "Click on extension to grant permissions.";
+  setTimeout(()=> background.style.display = "none", 2000);
+}
 
 
-
-(window as any).urbit = {
+(window as any).urbitVisor = {
   getShip: () => requestData("shipName"),
   requestPermissions: (permissions: LWURequest[]) => requestData("perms", permissions),
   scry: (payload: Scry) => requestData("scry", payload),
