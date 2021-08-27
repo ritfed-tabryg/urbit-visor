586a8a835d80bb4776ad332f811971f546fbd47a
mirtyl-wacdec <ycl2@protonmail.com>
2021-08-27 16:31:32 +0200

    Added icons, style fixes to settings popup

---
+++ src/icons/info.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function AboutIcon() {
+    return (
+<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" className="iconify iconify--carbon" width="24" height="24" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><defs></defs><path d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2zm0 6a1.5 1.5 0 1 1-1.5 1.5A1.5 1.5 0 0 1 16 8zm4 16.125h-8v-2.25h2.875v-5.75H13v-2.25h4.125v8H20z" fill="var(--foreground)"></path></svg>    )
+}
+
