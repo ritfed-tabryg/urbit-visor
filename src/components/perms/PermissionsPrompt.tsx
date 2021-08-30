573faaf69450f4a3937866d64a68570b9ba242c6
mirtyl-wacdec <ycl2@protonmail.com>
2021-08-30 19:14:26 +0200

    refreshed permissions prompts page, added removable chips

--- src/components/perms/PermissionsPrompt.tsx
+++ src/components/perms/PermissionsPrompt.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useHistory } from "react-router";
 import { AES } from "crypto-js";
 import { validate } from "../../storage";
 import "./perms.css";
-import { PermissionRequest } from "../../types/types"
+import { PermissionRequest, Permission } from "../../types/types"
 
 
 interface PermissionsProps {
@@ -14,15 +14,21 @@ interface PermissionsProps {
 export default function PermissionsPrompt(props: PermissionsProps) {
     console.log(props.perms, "perms")
     const history = useHistory();
+    const [perms, setPerms] = useState<Permission[]>([]);
     const [pw, setPw] = useState("");
     const [error, setError] = useState("");
-    
+    useEffect(()=> setPerms(props.perms.permissions), []);
+    function removePerm(perm: Permission){
+        const new_perms = perms.filter(p => p != perm);
+        setPerms(new_perms);
+    }
 
     async function grant() {
         const valid = await validate(pw);
         if (valid) {
             setError("");
-            props.savePerms(pw, props.perms);
+            const request = {website: props.perms.website, permissions: perms}
+            props.savePerms(pw, request);
             chrome.runtime.sendMessage({ type: "dismissPerms" });
             history.push("/");
         }
@@ -33,17 +39,25 @@ export default function PermissionsPrompt(props: PermissionsProps) {
         history.push("/")
     }
     return (
-        <div className="permissions">
-            <p>{props.perms.website} is requesting the following permissions: </p>
-            {props.perms.permissions.map(perm => {
-                return <p key={perm}>{perm}</p>
+        <div className="permissions padding flex-grow-wrapper">
+            <h3>Permissions Requested</h3>
+            <div className="flex-grow">
+            <a href={props.perms.website} rel="noopener noreferrer" target="_blank" className="requesting-domain">{props.perms.website}</a>
+            <p className="align-left">requested the following permissions: </p>
+            <ul>
+            {perms.map(perm => {
+                return <li key={perm}><Chip type={"new"} perm={perm} destroyPerm={removePerm} /></li>
             })}
-            {props.perms.existing && <Existing {...props}/>}
+            </ul>
+            {/* {props.perms.existing && <Existing {...props}/>} */}
             <p>Enter your master password to grant them.</p>
             <input onChange={(e) => setPw(e.currentTarget.value)} type="password" />
-            <button onClick={grant} type="submit">Grant</button>
-            <button className="red-bg" onClick={deny} type="submit">Deny</button>
             <p className="errorMessage">{error}</p>
+            </div>
+            <div className="two-buttons">
+            <button className="red-bg" onClick={deny} type="submit">Deny</button>
+            <button className="right" onClick={grant} type="submit">Grant</button>
+            </div>
         </div>
     )
 }
@@ -52,7 +66,28 @@ function Existing(props: PermissionsProps){
     return(
         <>
         <p>Following permissions already granted:</p>
-        {props.perms.existing.map(perm => <p key={perm}>{perm}</p>)}
+        <ul>
+        {props.perms.existing.map(perm => <li key={perm}><Chip type={"old"} perm={perm} /> </li>)}
+        </ul>
         </>
     )
+}
+type chipType = "new" | "old"
+interface ChipProps{
+    perm: Permission,
+    destroyPerm?: (perm: Permission) => void,
+    type: chipType
+}
+function Chip(props: ChipProps){
+    function destroy(){
+        console.log(props.perm)
+        props.destroyPerm(props.perm);
+    }
+  return(
+      <div className="chip">
+          <p>{props.perm}
+          {props.type == "new" && <span className="close "onClick={destroy}>x</span>}
+          </p>
+      </div>
+  )
 }
