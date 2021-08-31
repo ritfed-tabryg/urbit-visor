51d4e52dc2e0490f8f5138ad346357e282bae881
Robert Kornacki <11645932+robkorn@users.noreply.github.com>
2021-08-31 15:22:56 +0200

    About page cleanup

--- src/components/ui/About.tsx
+++ src/components/ui/About.tsx
@@ -3,15 +3,15 @@ import * as React from "react";
 export default function About(){
     return(
       <div className="about padding">
-         <div className="about-title"> 
+         <div className="about-title">
          <h4>Urbit Visor</h4>
-          <p>0.1.0</p>
           </div>
+          <p>Version: 0.1.0</p>
           <div className="about-description">
           <p>Urbit Visor is an overlay platform that merges Urbit and the web together. Its goal is to allow existing web tech to seamlessly integrate together with the novel functionality of Urbit.</p>
           </div>
           <div className="about-created-by">
-          <p>Created by:</p>
+          {/* <p>Created by:</p> */}
           <a href="https://dcspark.io" rel="noopener noreferrer" target="_blank">
           <img src="/dcsparklogo.png" alt="" />
           </a>
