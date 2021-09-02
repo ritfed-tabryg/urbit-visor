23f56b27fc253557eb12a598689117099a8ed679
mirtyl-wacdec <ycl2@protonmail.com>
2021-09-03 01:44:16 +0200

    fixed bug

--- src/components/adding/Confirm.tsx
+++ src/components/adding/Confirm.tsx
@@ -1,38 +1,38 @@
 import * as React from "react";
 import { useState } from "react";
-import {initPerms} from "../../urbit";
-import {getStorage, decrypt} from "../../storage";
+import { initPerms } from "../../urbit";
+import { getStorage, decrypt } from "../../storage";
 import Spinner from "../ui/svg/Spinner";
 
 
-interface ConfirmProps{
-    url: string,
-    code: string,
-    ship: string,
-    goBack: () => void,
-    save: (url: string, code: string, pw: string) => void
+interface ConfirmProps {
+  url: string,
+  code: string,
+  ship: string,
+  goBack: () => void,
+  save: (url: string, code: string, pw: string) => void
 }
-export default function Confirm(props: ConfirmProps){
+export default function Confirm(props: ConfirmProps) {
   const [loading, setLoading] = useState(false);
   const [pw, setPw] = useState("");
   const [error, setError] = useState("");
   const spinner = <Spinner width="24" height="24" innerColor="white" outerColor="black" />
 
-    function addShip(e: React.FormEvent<HTMLFormElement>){
-      e.preventDefault();
-      getStorage("password")
-      .then((res) =>{
-         const string = decrypt(res.password, pw);
-         if(string === "urbit_visor"){
-           saveShip()
-         }else{
-           setError("Wrong password.")
-         }
+  function addShip(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    getStorage("password")
+      .then((res) => {
+        const string = decrypt(res.password, pw);
+        if (string === "urbit_visor") {
+          saveShip()
+        } else {
+          setError("Wrong password.")
+        }
       })
-    }    
-    function saveShip(){
-      setLoading(true)
-      initPerms(props.ship, props.url)
+  }
+  function saveShip() {
+    setLoading(true)
+    initPerms(props.ship, props.url)
       .then((res) => {
         props.save(props.url, props.code, pw);
         setLoading(false)
@@ -41,23 +41,25 @@ export default function Confirm(props: ConfirmProps){
         setLoading(false)
         setError("Error adding the ship, please try again.")
       })
-    };
-    return(
-      <div className="confirm padding flex-grow-wrapper">
+  };
+  const hidden = {display: "none"};
+  return (
+    <div className="confirm padding flex-grow-wrapper">
       <p>Connection successful to:</p>
       <p className="confirm-shipname">~{props.ship} </p>
       <p>Please confirm your master password.</p>
       <form onSubmit={addShip} className="form confirm-form flex-grow-wrapper">
-      <div className="flex-grow">
-      <input value={pw} onChange={(e)=> setPw(e.currentTarget.value)} type="password" />
-      {loading && spinner}
-      <p className="errorMessage">{error}</p>
-      </div>
-      <div className="two-buttons">
-      <button className="small-button red-bg" onClick={props.goBack}>Cancel</button>
-      <button type="submit" className="small-button right">Submit</button>
-      </div>
+      <input type="submit" style={hidden}/>
+        <div className="flex-grow">
+          <input value={pw} onChange={(e) => setPw(e.currentTarget.value)} type="password" />
+          {loading && spinner}
+          <p className="errorMessage">{error}</p>
+        </div>
+        <div className="two-buttons">
+          <button className="small-button red-bg" onClick={props.goBack}>Cancel</button>
+          <button type="submit" className="small-button right">Submit</button>
+        </div>
       </form>
-      </div>
-    )
-  }
+    </div>
+  )
+}
